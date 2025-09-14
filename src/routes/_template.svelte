b996062c0e557f48af3aa24885091f22027766e7
mhkeller <code@mhkeller.com>
2025-09-14 18:42:27 +0100

    add template

---
+++ src/routes/_template.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Maplibre from '$lib/Maplibre.svelte';
+	import loadConfig from '$lib/modules/loadConfig.js';
+
+	/** imports **/
+
+	const config = loadConfig(/** layers **/);
+</script>
+
+<Maplibre {config} />
